dd45d3994e540741076d5d767287e5f99c25e427
NexoCeo <NexoCeoGroup@gmail.com>
2026-09-03 08:51:10 -0300

    Refactor global routes to include navigation

--- src/routes/globalRoutes.tsx
+++ src/routes/globalRoutes.tsx
@@ -1,14 +1,56 @@
-import { Routes, Route } from "react-router-dom";
+import {
+  Navigate,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-import Login from '@/pages/auth/signin';
-import Register from "@/pages/auth/signup/index";
-import RecuperarSenha from "@/pages/auth/forgotPassword"
+import Login from "@/pages/auth/signin";
+import Register from "@/pages/auth/signup";
+import RecuperarSenha from "@/pages/auth/forgotPassword";
+
+import { getEntryPath } from "./session-paths";
+
 export default function GlobalRoutes() {
+  const entryPath = getEntryPath();
+
   return (
     <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="recuperarSenha" element={<RecuperarSenha />} />
+      {/* Ao acessar /global */}
+      <Route
+        index
+        element={
+          <Navigate
+            to={entryPath}
+            replace
+          />
+        }
+      />
+
+      <Route
+        path="login"
+        element={<Login />}
+      />
+
+      <Route
+        path="register"
+        element={<Register />}
+      />
+
+      <Route
+        path="recuperarSenha"
+        element={<RecuperarSenha />}
+      />
+
+      {/* Rota global inexistente */}
+      <Route
+        path="*"
+        element={
+          <Navigate
+            to={entryPath}
+            replace
+          />
+        }
+      />
     </Routes>
-  )
+  );
 }
